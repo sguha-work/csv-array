d5fcaf6e6b1db9a83287b4596899281119dfed3d
sguha-work <sguha1988.life@gmail.com>
2015-04-20 18:39:52 +0530

    continues

--- csv-parse.js
+++ csv-parse.js
@@ -12,6 +12,37 @@ module.exports = {
 	},
 
 	parseFile : function(fileName, callBack) {
+		var presentInstance = this;
+		var fs = require('fs');
+		fs.readFile(fileName, 'utf-8', function(data) {
+			var dataArray = presentInstance.getDataSeparatedByNewLine(data);
+			var finalDataArray = presentInstance.getDataArray(dataArray);
+			callBack(finalDataArray);
+		})
+	},
+
+	getDataSeparatedByNewLine : function(data) {
+		var dataArray = data.split("\n");
+		return dataArray;
+	},
+
+	getDataArray : function(dataArray) {
+		presentInstance = this;
+		var attributeNameArray = dataArray[0].split(",");
+		var finalArray = [];
+		for(var index=1; index<dataArray.length; index++) {
+			var tempArray = [];
+			var dataList = presentInstance.getDataFromLine(dataArray[index]);
+			for(var index2=0; index2<attributeNameArray.length; index2++) {
+				tempArray[attributeNameArray[index2]] = dataList[index2];
+			}
+			finalArray.push(tempArray);
+		}
+		return finalArray;
+
+	},
+
+	getDataFromLine : function(line) {
 		
 	}
 
