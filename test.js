191157a9db045196dcf9d5a11fed77c149580b70
sguha-work <sguha1988.life@gmail.com>
2015-04-24 10:38:56 +0530

    small bug fix

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var csv = require('./csv-array');
 var fs = require('fs');
-// csv.parseCSV("test.csv", function(data){
-// 	fs.writeFile("test.json", JSON.stringify(data), function(){console.log("done")});
-// });
+csv.parseCSV("test.csv", function(data){
+	fs.writeFile("test.json", JSON.stringify(data), function(){console.log("done")});
+});
 
 // csv.parseCSV("test2.csv", function(data){
 // 	fs.writeFile("test2.json", JSON.stringify(data), function(){console.log("done")});
