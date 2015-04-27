67ae3912a97b640585995c259083e98005ab0b05
sguha-work <sguha1988.life@gmail.com>
2015-04-27 17:20:13 +0530

    speed improvement

--- test.js
+++ test.js
@@ -2,14 +2,14 @@ var csv = require('./csv-array');
 var fs = require('fs');
 // csv.parseCSV("test.csv", function(data){
 // 	fs.writeFile("test.json", JSON.stringify(data), function(){console.log("done")});
-// }, false);
+// }, true);
 
 // csv.parseCSV("test2.csv", function(data){
 // 	fs.writeFile("test2.json", JSON.stringify(data), function(){console.log("done")});
 	
-// },false);
+// },true);
 
 csv.parseCSV("test3.csv", function(data){
 	fs.writeFile("test3.json", JSON.stringify(data), function(){console.log("done")});
 	
-},true);
+},false);
